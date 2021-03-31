2e4712db6922c4543fa2ace1afc481ce0193ba52
LinaM20 <c19366191@mytudublin.ie>
2021-03-31 22:17:06 +0100

    base code

--- java/src/C19366191/LinasVisuals.java
+++ java/src/C19366191/LinasVisuals.java
@@ -1,5 +1,31 @@
 package C19366191;
 
-public class LinasVisuals {
+import ie.tudublin.Visual;
+
+public class LinasVisuals extends Visual
+{
+    public void settings()
+    {
+        size(1024, 500);
+    }
     
+    public void setup()
+    {
+        startMinim();
+        loadAudio("Punjabi.mp3");
+    }
+
+    public void keyPressed()
+    {
+        if (key == ' ')
+        {
+            getAudioPlayer().cue(0);
+            getAudioPlayer().play();
+        }
+    }
+
+    public void draw()
+    {
+        background(0);
+    }
 }
