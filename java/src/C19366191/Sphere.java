190a6637e15d655425189fe72e20e89c22c36d3f
LinaM20 <c19366191@mytudublin.ie>
2021-04-03 15:39:57 +0100

    added a cube and removed the pyramid from the sphere

--- java/src/C19366191/Sphere.java
+++ java/src/C19366191/Sphere.java
@@ -43,45 +43,11 @@ public class Sphere extends PApplet
         
         lv.noFill();
         
+        //Makes the sphere spin
         boxSize = 10 + (lv.getAmplitude() * 500);
-
         insideSphere = lerp(insideSphere, boxSize, 0.1f);    
 
-        //Makes the sphere spin
-        p = 5 + (lv.getAmplitude() * 100);
-        t = lerp(t, p, 0.05f); 
-        
         lv.pushMatrix();
-
-        lv.rotateX(theta);
-        lv.rotateY(theta);
-        lv.rotateZ(theta);
-        //lv.translate(0,0, 0);
-        //lv.noFill();
-
-        lv.strokeWeight(3);
-        
-        lv.beginShape();
-        
-        
-        lv.vertex(-t, -t, -t);
-        lv.vertex(t, -t, -t);
-        lv.vertex(   0,    0,  t);
-
-        lv.vertex(t, -t, -t);
-        lv.vertex( t,  t, -t);
-        lv.vertex(   0,    0, t);
-
-        lv.vertex( t, t, -t);
-        lv.vertex(-t, t, -t);
-        lv.vertex(   0,   0,  t);
-
-        lv.vertex(-t,  t, -t);
-        lv.vertex(-t, -t, -t);
-        lv.vertex(   0,    0,  t);
-        lv.endShape();
-
-        theta += 0.01f;
 
         lv.translate(0, 0, 0); //places sphere in the middle
         lv.rotateY(angle);
@@ -93,7 +59,6 @@ public class Sphere extends PApplet
         lv.popMatrix();
 
        
-        
 
         angle += 0.01f; //rotates the sphere
 
